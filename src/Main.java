1a186fe16e4dd8636e2d24c73f0cfb7737d7cafa
Giovana Nascimento de Souza <giovananaunifil@edu.unifil.br>
2024-10-16 10:54:49 -0300

    iniciando commits

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro("T-Rex");
+        cachorro.dormir();
+        cachorro.fazerSom();
+
+    }
+}
